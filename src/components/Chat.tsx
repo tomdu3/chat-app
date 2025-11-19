273d343d00c903c9e60801a9d5d1dcc79d65b91f
Tomislav Dukez <tomdu3@ymail.com>
2025-11-19 05:28:30 +0000

    fix Chakra components to v3

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,53 +1,55 @@
 "use client";
 
 import { ColorModeButton } from "@/components/ui/color-mode";
-import { Box, Flex, VStack, HStack, Text, Input, IconButton, Avatar } from "@chakra-ui/react";
+import { Box, Flex, Stack, Text, Input, IconButton, Avatar } from "@chakra-ui/react";
 import { LuSend } from "react-icons/lu";
 
 export function Chat() {
   return (
     <Flex h="100vh">
-      <VStack w="20%" bg="gray.900" p={4} spacing={4} alignItems="flex-start">
+      <Stack w="20%" bg="gray.900" p={4} gap={4} alignItems="flex-start" direction="column">
         <Text fontSize="xl" fontWeight="bold">
           Chat Rooms
         </Text>
-        <VStack spacing={2} alignItems="flex-start">
+        <Stack gap={2} alignItems="flex-start" direction="column">
           <Text># general</Text>
           <Text># random</Text>
           <Text># tech</Text>
-        </VStack>
+        </Stack>
         <Box flex="1" />
         <ColorModeButton />
-      </VStack>
+      </Stack>
       <Flex flex="1" direction="column">
-        <VStack flex="1" p={4} spacing={4} alignItems="flex-start">
-          <HStack>
+        <Stack flex="1" p={4} gap={4} alignItems="flex-start" direction="column">
+          <Stack direction="row">
             <Avatar.Root>
               <Avatar.Fallback>U1</Avatar.Fallback>
             </Avatar.Root>
-            <VStack alignItems="flex-start" spacing={1}>
+            <Stack alignItems="flex-start" gap={1} direction="column">
               <Text fontWeight="bold">User 1</Text>
               <Text bg="gray.700" p={2} borderRadius="md">
                 Hello!
               </Text>
-            </VStack>
-          </HStack>
-          <HStack alignSelf="flex-end">
-            <VStack alignItems="flex-end" spacing={1}>
+            </Stack>
+          </Stack>
+          <Stack alignSelf="flex-end" direction="row">
+            <Stack alignItems="flex-end" gap={1} direction="column">
               <Text fontWeight="bold">You</Text>
               <Text bg="blue.500" p={2} borderRadius="md">
                 Hi there!
               </Text>
-            </VStack>
+            </Stack>
             <Avatar.Root>
               <Avatar.Fallback>Y</Avatar.Fallback>
             </Avatar.Root>
-          </HStack>
-        </VStack>
-        <HStack p={4}>
+          </Stack>
+        </Stack>
+        <Stack p={4} direction="row">
           <Input placeholder="Type a message..." />
-          <IconButton aria-label="Send message" icon={<LuSend />} />
-        </HStack>
+          <IconButton aria-label="Send message">
+            <LuSend />
+          </IconButton>
+        </Stack>
       </Flex>
     </Flex>
   );
