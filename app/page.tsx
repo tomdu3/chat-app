2296da5428565466e27bfabc90a41033d02349ab
Tomislav Dukez <tomdu3@ymail.com>
2025-12-15 19:47:10 +0000

    add install convex db and clerk auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Home() {
+  const [a, setA] = useState(false)
   return <Button size="lg">
     Button
   </Button>;
