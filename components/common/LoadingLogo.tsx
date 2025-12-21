3307dd8d6ecfcd2e63bcbf06897051a4b9a32167
Tomislav Dukez <tomdu3@ymail.com>
2025-12-21 06:17:49 +0000

    fix hydration errors for LoadingLogo component

--- components/common/LoadingLogo.tsx
+++ components/common/LoadingLogo.tsx
@@ -8,7 +8,7 @@ type Props = {
 export function LoadingLogo({ size = 100 }: Props) {
     return (
         <div className="h-full w-full flex justify-center items-center">
-            <Image src="/imgs/logo.svg" className="animate-spin duration-600" width={size} height={size} alt="Logo" />
+            <Image loading="eager" src="/imgs/logo.svg" className="animate-spin duration-600" width={size} height={size} alt="Logo" />
         </div>
     )
 }
